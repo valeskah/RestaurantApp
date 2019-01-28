cf5a0673ae6bfadc691715b7a8592182c272a92d
Valeska Halamicek <vjhalamicek@gmail.com>
2019-01-27 23:13:44 -0600

    Very simple started code

---
+++ RestaurantApp/RestaurantApp/Controllers/RestaurantController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestaurantApp.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        static private Dictionary<string, string> Restaurants = new Dictionary<string, string>();
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+
+            ViewBag.restaurants = Restaurants;
+
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [Route("Restaurant/Add")]
+        public IActionResult NewRestaurant(string name, string notes = "")
+        {
+            //Add new restaurant to restaurant list
+            Restaurants.Add(name, notes);
+
+            return Redirect("/Restaurant");
+        }
+    }
+}
